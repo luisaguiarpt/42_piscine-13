5ab9d3b0ae92b3587186c021be3d21e60dcb40f2
luisaguiar <luisaguiar@duck.com>
2025-03-23 17:59:15 +0000

    added ft_any

--- c11/ex02/ft_any.c
+++ c11/ex02/ft_any.c
@@ -1,19 +1,27 @@
 int	ft_any(char **tab, int(*f)(char*))
 {
-	
+	while (*tab)
+		if (!((*f)(*tab++)))
+			return (0);
+	return (1);
 }
 
 int	is_lower(char *str)
 {
 	while (*str)
-		if (*str >= 'A' || *str <= 'Z')
-			return (1);
-	return (0);
+	{
+		if (*str >= 'A' && *str <= 'Z')
+			return (0);
+		str++;
+	}
+	return (1);
 }
+
+#include <stdio.h>
 
 int main(void)
 {
-	char **tab = {"hello", "im", "giid"};
+	char *tab[4] = {"hello", "im", " luis", 0};
 
-
+	printf("%i\n", ft_any(tab, *is_lower));	
 }
